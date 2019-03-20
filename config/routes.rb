2e4bd88ca0c8564e804b5c0362faa000b2265c71
Alexandra Bryan <alexandratbryan@gmail.com>
2019-03-20 00:17:02 -0400

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
-<<<<<<< Updated upstream
-  get 'home/index'
-    root 'application#hello'
-=======
   devise_for :users
   root 'home#index'
   get "/" => "home#index"
   get "about" => "home#about"
->>>>>>> Stashed changes
 end
