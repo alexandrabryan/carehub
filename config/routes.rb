fa094a4e58ab4cb5997331e56caf441e68804ff7
Alexandra Bryan <alexandratbryan@gmail.com>
2019-03-19 22:54:58 -0400

    generate devise user

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+<<<<<<< Updated upstream
   get 'home/index'
     root 'application#hello'
+=======
+  devise_for :users
+  root 'home#index'
+  get "/" => "home#index"
+  get "about" => "home#about"
+>>>>>>> Stashed changes
 end
