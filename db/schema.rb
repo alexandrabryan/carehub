7c66667b0da430d1a360fb9be086228608fae2f2
Alexandra Bryan <alexandratbryan@gmail.com>
2019-03-21 00:55:13 -0400

    generate migration file to add name column to user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_20_182001) do
+ActiveRecord::Schema.define(version: 2019_03_21_044740) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
 
   create_table "events", force: :cascade do |t|
     t.string "name"
     t.datetime "start_time"
     t.datetime "end_time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -32,6 +38,7 @@ ActiveRecord::Schema.define(version: 2019_03_20_182001) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.where(email: "thecarehubapp@gmail.com").first_or_create.update_attributes(nome: "Care Hub App Team",
+User.where(email: "thecarehubapp@gmail.com").first_or_create.update_attributes(name: "Care Hub App Team",
     email: "thecarehubapp@gmail.com",
     password:              "devcamp2",
     password_confirmation: "devcamp2")
