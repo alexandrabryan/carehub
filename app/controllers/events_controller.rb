02f31945d018de568e6d028e81c620914f9c80a3
Nicole Spoto <nicolespoto@gmail.com>
2019-03-22 11:45:42 -0400

    Toggle b/w calendar views (day/week/month)

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,6 +7,14 @@ class EventsController < ApplicationController
     @events = Event.all
   end
 
+  def day
+    @events = Event.all
+  end
+
+  def week
+    @events = Event.all
+  end
+
   # GET /events/1
   # GET /events/1.json
   def show
