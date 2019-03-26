1868af4300eb364b8530482c511986b4eddd8d2e
Alexandra Bryan <alexandratbryan@gmail.com>
2019-03-26 18:49:56 -0400

    add name and user reference to Groups migration

--- db/migrate/20190326162329_create_groups.rb
+++ db/migrate/20190326162329_create_groups.rb
@@ -1,6 +1,8 @@
 class CreateGroups < ActiveRecord::Migration[5.2]
   def change
     create_table :groups do |t|
+      t.string :name
+      t.references :user, foreign_key: true
 
       t.timestamps
     end
