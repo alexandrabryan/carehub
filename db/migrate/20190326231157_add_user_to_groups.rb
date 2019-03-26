c3a78a9abdb11607f4b8852e4afb1befeee1c9e4
Alexandra Bryan <alexandratbryan@gmail.com>
2019-03-26 19:14:33 -0400

    create migration to add name and user reference to Groups table

---
+++ db/migrate/20190326231157_add_user_to_groups.rb
@@ -0,0 +1,5 @@
+class AddUserToGroups < ActiveRecord::Migration[5.2]
+  def change
+    add_reference :groups, :user, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_26_162443) do
+ActiveRecord::Schema.define(version: 2019_03_26_231157) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,9 @@ ActiveRecord::Schema.define(version: 2019_03_26_162443) do
   create_table "groups", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_groups_on_user_id"
   end
 
   create_table "items", force: :cascade do |t|
@@ -68,6 +71,7 @@ ActiveRecord::Schema.define(version: 2019_03_26_162443) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "groups", "users"
   add_foreign_key "memberships", "groups"
   add_foreign_key "memberships", "users"
 end
