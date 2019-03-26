3d95bf974e98f127af9272106df42324d109b5e1
Alexandra Bryan <alexandratbryan@gmail.com>
2019-03-26 12:24:56 -0400

    generate Membership model

---
+++ db/migrate/20190326162443_create_memberships.rb
@@ -0,0 +1,10 @@
+class CreateMemberships < ActiveRecord::Migration[5.2]
+  def change
+    create_table :memberships do |t|
+      t.references :group, foreign_key: true
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
