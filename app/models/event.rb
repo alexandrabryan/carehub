aa194fc2ec4905731a4c486ca6f91119d234f289
Nicole Spoto <nicolespoto@gmail.com>
2019-03-26 17:39:21 -0400

    Display username on posts and events; remove table under posts

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,6 +5,10 @@ class Event < ApplicationRecord
   validate :end_date_after_start_date
   validate :start_date_after_now
 
+  # Define the "user" instance method
+  def user
+    return User.find_by(id: self.user_id)
+  end
 
   def end_date_after_start_date
       return if end_time.blank? || start_time.blank?
