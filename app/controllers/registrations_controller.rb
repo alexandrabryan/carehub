9afe55b422337f7ab63833357f080156d84eebee
Nicole Spoto <nicolespoto@gmail.com>
2019-03-27 14:58:20 -0400

    Add ability to create or join group from signup

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,22 @@
+class RegistrationsController < Devise::RegistrationsController
+
+  def create
+
+    if !params.has_key?(:group_id)
+
+      group = Group.create!
+      #@group.create
+      #@group.save
+      params[:user][:group_id] = group.id
+    end
+
+    # See what the parameter are in the console
+    #puts params[:user]
+
+    # call the devise controller's create method
+    super
+
+  end
+
+
+end
