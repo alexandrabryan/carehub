b39305a56baa4f34f1e33005daa7f9ab08c782c9
Nicole Spoto <nicolespoto@gmail.com>
2019-03-27 01:22:04 -0400

    Add record to membership table on group create

--- db/schema.rb
+++ db/schema.rb
@@ -64,11 +64,7 @@ ActiveRecord::Schema.define(version: 2019_03_27_032235) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
     t.bigint "group_id"
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["group_id"], name: "index_users_on_group_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
